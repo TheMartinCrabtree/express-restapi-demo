7d797d40a106bb3145d4c71be9963b5fd77a33fb
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-23 14:30:57 -0400

    setting up env files

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-import userData from "./data";
-
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3003;
 
 
 const handleGet=(req, res)=>{
     res.send("Root Get Request");
-    
+}
+
+const getAllUsers=(req, res)=>{
+    res.send([1,2,3,4])
 }
 
 // ROUTES
 app.get("/", handleGet);
 
+app.get("/users", getAllUsers);
 
-app.listen(3003, ()=>console.log("listening on port 3003"));
+// 
+
+app.listen(port, ()=>console.log(`listening on port: ${port}`));
 
