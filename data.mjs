f998994d18c33cf705ed8b29d8363b1603f621db
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-23 16:30:01 -0400

    pulling data from get users and user

---
+++ data.mjs
@@ -0,0 +1,40 @@
+const userData = [
+    {
+        "id": 1,
+        "name_first": "Adi",
+        "name_last": "Tariq",
+        "subscriber": true,
+        "comments": 7
+    },
+    {
+        "id": 2,
+        "name_first": "Michelle",
+        "name_last": "Tran",
+        "subscriber": false,
+        "comments": 2
+    },
+    {
+        "id": 3,
+        "name_first": "Angela",
+        "name_last": "Mann",
+        "subscriber": true,
+        "comments": 10
+    },
+    {
+        "id": 4,
+        "name_first": "Daphne",
+        "name_last": "Chen",
+        "subscriber": false,
+        "comments": 4
+    },
+    {
+        "id": 5,
+        "name_first": "Amanda",
+        "name_last": "Burger",
+        "subscriber": true,
+        "comments": 0
+    }
+    
+  ]
+
+  export default userData;
--- index.js
+++ index.js
@@ -2,6 +2,45 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.listen(port, ()=>console.log(`listening on port: ${port}`));
+
+const userData = [
+    {
+        "id": 1,
+        "name_first": "Adi",
+        "name_last": "Tariq",
+        "subscriber": true,
+        "comments": 7
+    },
+    {
+        "id": 2,
+        "name_first": "Michelle",
+        "name_last": "Tran",
+        "subscriber": false,
+        "comments": 2
+    },
+    {
+        "id": 3,
+        "name_first": "Angela",
+        "name_last": "Mann",
+        "subscriber": true,
+        "comments": 10
+    },
+    {
+        "id": 4,
+        "name_first": "Daphne",
+        "name_last": "Chen",
+        "subscriber": false,
+        "comments": 4
+    },
+    {
+        "id": 5,
+        "name_first": "Amanda",
+        "name_last": "Burger",
+        "subscriber": true,
+        "comments": 0
+    }
+  ]
 
 const handleGet=(req, res)=>{
     res.send("Root Get Request");
@@ -12,18 +51,27 @@ const getAllUsers=(req, res)=>{
 }
 
 const getUser=(req, res)=>{
-    // read request params
-    res.send(req.params.id);
+    // read request req.params and/or req.query
+    const userObj = userData.find((user)=>{
+        console.log("user:", user.id)
+        debugger
+        return user.id === parseInt(req.params.id);
+    })
+    if(!userObj){
+        res.status(404).send("User not found.");
+    }
+    else{
+        res.send(userObj);
+    }
+
 }
 
-// ROUTES
+// ROUTES 
 app.get("/", handleGet);
 
 app.get("/users", getAllUsers);
 
 app.get("/user/:id", getUser);
 
-// 
 
-app.listen(port, ()=>console.log(`listening on port: ${port}`));
 
