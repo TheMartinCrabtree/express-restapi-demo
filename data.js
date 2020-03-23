5a48aadcd0869bb17956795f4342ec6f3385d593
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-23 14:07:46 -0400

    basic setup for routes

--- data.js
+++ data.js
@@ -1,33 +1,34 @@
-[
+const data = [
     {
+        "id": 1,
         "name_first": "Adi",
         "name_last": "Tariq",
         "subscriber": true,
         "comments": 7
     },
     {
-
+        "id": 2,
         "name_first": "Michelle",
         "name_last": "Tran",
         "subscriber": false,
         "comments": 2
     },
     {
-
+        "id": 3,
         "name_first": "Angela",
         "name_last": "Mann",
         "subscriber": true,
         "comments": 10
     },
     {
-
+        "id": 4,
         "name_first": "Daphne",
         "name_last": "Chen",
         "subscriber": false,
         "comments": 4
     },
     {
-
+        "id": 5,
         "name_first": "Amanda",
         "name_last": "Burger",
         "subscriber": true,
@@ -35,3 +36,5 @@
     }
     
   ]
+
+  export default data;
--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const NODE_ENV = process.env.NODE_ENV || 'development';
+import userData from "./data";
 
-app.set('port', PORT);
-app.set('env', NODE_ENV);
-app.use(logger('tiny'));
-app.use(bodyParser.json());
-app.use('/', require(path.join(__dirname, 'routes')));
-app.use((req, res, next) => {
-  const err = new Error(`${req.method} ${req.url} Not Found`);
-  err.status = 404;
-  next(err);
-});
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message,
-    },
-  });
-});
-app.listen(PORT, () => {
-  console.log(
-    `Express Server started on Port ${app.get(
-      'port'
-    )} | Environment : ${app.get('env')}`
-  );
-});
+const express = require('express');
+const app = express();
+
+
+
+const handleGet=(req, res)=>{
+    res.send("Root Get Request");
+    
+}
+
+// ROUTES
+app.get("/", handleGet);
+
+
+app.listen(3003, ()=>console.log("listening on port 3003"));
+
